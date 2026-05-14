0a84730818ee9ccfdd70f0ef3e0ee772284bcd78
Rank-8_RK <djcraes28@gmail.com>
2026-05-14 11:35:15 +0200

    Create exporter.js

---
+++ js/exporter.js
@@ -0,0 +1,26 @@
+class JSONExporter {
+
+  static download(data, filename = "rk_opening_tree.json") {
+
+    const jsonString = JSON.stringify(data, null, 2);
+
+    const blob = new Blob([jsonString], {
+      type: "application/json"
+    });
+
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement("a");
+
+    link.href = url;
+    link.download = filename;
+
+    document.body.appendChild(link);
+
+    link.click();
+
+    document.body.removeChild(link);
+
+    URL.revokeObjectURL(url);
+  }
+}
